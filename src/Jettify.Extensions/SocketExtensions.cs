667ae3fe9f76cd8962794762f0add00164ef9174
Егор Зеленский <i@khaos.su>
2016-06-29 16:50:16 +0300

    Add extensions for sockets and `TcpClient`

---
+++ src/Jettify.Extensions/SocketExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Jettify.Extensions {
+    /// <summary>
+    /// Provides extensions for <see cref="Socket"/> objects.
+    /// </summary>
+    public static class SocketExtensions {
+        public const int MaxUdpDatagramSize = 0x10000;
+
+        public static NetworkStream GetStream(this Socket s, bool ownsSocket = false) {
+            return new NetworkStream(s, ownsSocket);
+        }
+
+        public static int PollRead(this Socket s, byte[] buffer, int microsecondsTimeout) {
+            if (s.Poll(microsecondsTimeout, SelectMode.SelectRead)) {
+                return s.Receive(buffer);
+            }
+
+            return -1;
+        }
+
+        public static bool IsReallyConnected(this Socket s, int timeout = 100) {
+            return !((s.Poll(timeout, SelectMode.SelectRead) && (s.Available == 0)) || !s.Connected);
+        }
+    }
+}
---
+++ src/Jettify.Extensions/TcpClientExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Jettify.Extensions {
+    /// <summary>
+    /// Provides extension methods for <see cref="TcpClient"/> objects.
+    /// </summary>
+    public static class TcpClientExtensions {
+        public static void ConnectWithTimeout(this TcpClient client, string hostname, int port, int timeout = 5000) {
+            var task = client.ConnectAsync(hostname, port);
+
+            try {
+                task.Wait(timeout);
+            }
+            catch (AggregateException e) {
+                throw e.InnerException;
+            }
+
+            if (!task.IsCompleted) {
+                throw new TimeoutException("Connect timeout.");
+            }
+        }
+    }
+}
