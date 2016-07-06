8ec563115d9b562d65b50111b91f170cbfa82a73
Егор Зеленский <i@khaos.su>
2016-07-06 16:42:21 +0300

    Add server usage example stub

--- src/TestSandbox/Program.cs
+++ src/TestSandbox/Program.cs
@@ -1,7 +1,14 @@
-﻿namespace TestSandbox {
+﻿using Jettify.Serve;
+
+namespace TestSandbox {
     public class Program {
         public static void Main(string[] args) {
-            
+            using (var server = new TcpThreadedServerFunc(7777, (s) => {
+
+            })) {
+                server.Start();
+                server.WaitForJoin();
+            }
         }
     }
 }
